5d5c779a16ec127a0ba75f40bb776d90cd35fe14
carinakn <carinaknics@gmail.com>
2023-10-22 17:03:08 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger');
+  const currentPosition = parseInt(dodger.style.left);
+
+  const newPosition = currentPosition - 1;
+
+  dodger.style.left = `${newPosition}px`;
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  const currentPosition = parseInt(dodger.style.left);
+
+  const newPosition = currentPosition + 1;
+
+  dodger.style.left = `${newPosition}px`;
+}
